98a0cb38465237281abbe65dd3925cc54fcb8699
Ronny Krite <rkrite@gmail.com>
2022-04-25 09:03:57 +1000

    added ga

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -24,6 +24,18 @@
         <script src="./js/custom.js{{ GForceNoCache(['force'=>1]) }}"></script>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8TDGHT4F88"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-8TDGHT4F88');
+        </script>
+
     </head>
 
     <body>
